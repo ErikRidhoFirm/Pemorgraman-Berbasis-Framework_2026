5d55624228338e24219e48ad23a2a23a9667572e
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-02 15:31:09 +0700

    telah menyelesaikan jobsheet 4

--- src/pages/category/[...slug].tsx
+++ src/pages/category/[...slug].tsx
@@ -1,17 +1,17 @@
 import { useRouter } from "next/router";
 
 const halamanCategory = () => {
-    // const Router = useRouter;
-    // console.log(Router);
-    const { query } = useRouter();
-    const { slug } = query;
-    return (
-        <div>
-            <h1> Halaman Category</h1>
-            <p>Category: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
-            <p>Kategori: {slug ? slug[0]: "Semua Kategori"}</p>
-        </div>
-    );
+  // const Router = useRouter;
+  // console.log(Router);
+  const { query } = useRouter();
+  const { slug } = query;
+  return (
+    <div>
+      <h1> Halaman Category</h1>
+      <p>Category: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
+      <p>Kategori: {slug ? slug[0] : "Semua Kategori"}</p>
+    </div>
+  );
 };
 
 export default halamanCategory;
